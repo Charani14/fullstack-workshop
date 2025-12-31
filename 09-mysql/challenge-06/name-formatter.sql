04840c23d121305f2424a1f8815facf660f78423
Sri Charani Gudipudi <charanigudipudi1024@gmail.com>
2025-12-31 15:55:15 +0530

    completed sql challenges

---
+++ 09-mysql/challenge-06/name-formatter.sql
@@ -0,0 +1,7 @@
+
+SELECT
+    CONCAT(UPPER(SUBSTRING_INDEX(name, ' ', -1)), ', ', SUBSTRING_INDEX(name, ' ', 1)) AS formatted_name,
+    CONCAT(LOWER(SUBSTRING_INDEX(name, ' ', 1)), '.', LOWER(SUBSTRING_INDEX(name, ' ', -1)), '@revature.com') AS email,
+    CONCAT(UPPER(LEFT(SUBSTRING_INDEX(name, ' ', 1), 1)), UPPER(LEFT(SUBSTRING_INDEX(name, ' ', -1), 1))) AS initials
+FROM
+    revature.employees;
