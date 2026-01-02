cb2901eedc2a6eec41f69924b5bfac9a4ee05f88
Sri Charani Gudipudi <charanigudipudi1024@gmail.com>
2026-01-02 16:08:02 +0530

    changed a sql file

--- 09-mysql/challenge-01/department-stats.sql
+++ 09-mysql/challenge-01/department-stats.sql
@@ -1,8 +1,8 @@
 SELECT 
     e.department,
-    COUNT(e.id) AS employee_count,
+    COUNT(*) AS employee_count,
     ROUND(AVG(e.salary), 2) AS avg_salary,
     MAX(e.salary) AS max_salary
 FROM employees e
 GROUP BY e.department
-HAVING COUNT(e.id) > 2;
+HAVING COUNT(*) > 2;
